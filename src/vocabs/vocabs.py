beabf9932f47431c52658b1699f4ce676b661490
Ingeborg van Vugt <ingeborgvanvugt@hotmail.com>
2023-09-20 13:56:27 +0200

    nwo research fields
    Added nwo-research-fields

--- src/vocabs/vocabs.py
+++ src/vocabs/vocabs.py
@@ -21,7 +21,6 @@ for result_entry in domains_data['result']:
                 # Update the entry with the "skos:exactMatch" field in the desired format
                 entry['skos:exactMatch'] = f"{result_entry['index']} {result_entry['title']}"
 
-
 # Save the updated research_fields_data back to the "now-research-fields.json" file
 with open('now-research-fields_template.json', 'w') as research_fields_file:
     json.dump(nwo_data, research_fields_file, indent=4)
